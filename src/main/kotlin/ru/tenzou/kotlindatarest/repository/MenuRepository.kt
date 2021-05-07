72a9ce033e45c64e14e9f3d30149ea9f693d0581
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-07 13:41:39 +0300

    Add rest repository

---
+++ src/main/kotlin/ru/tenzou/kotlindatarest/repository/MenuRepository.kt
@@ -0,0 +1,8 @@
+package ru.tenzou.kotlindatarest.repository
+
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import ru.tenzou.kotlindatarest.model.Menu
+
+@RepositoryRestResource
+interface MenuRepository : JpaRepository<Menu, Long>
