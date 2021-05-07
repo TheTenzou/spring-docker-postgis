4a9a18f24fca45dca4f22be932f79f288802d3d1
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-05-07 12:07:39 +0300

    Initial commit

---
+++ src/main/kotlin/ru/tenzou/kotlindatarest/KotlinDataRestApplication.kt
@@ -0,0 +1,11 @@
+package ru.tenzou.kotlindatarest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDataRestApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinDataRestApplication>(*args)
+}
---
+++ src/test/kotlin/ru/tenzou/kotlindatarest/KotlinDataRestApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.tenzou.kotlindatarest
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinDataRestApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
